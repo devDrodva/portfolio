a7fad5623e1a523f8ce03eed16807db207d5cdcb
devDrova <dvargas0720@gmail.com>
2021-09-15 12:52:17 -0600

    first-commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a href='https://www.linkedin.com/in/drodva/' className='link footer__link'>
+      David Rodriguez
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,45 @@
+const header = {
+  homepage: '',
+  title: '',
+}
+
+const about = {
+  name: 'David Rodriguez',
+  role: 'Front-end Developer',
+  description:
+    'I have experience writing web apps. I have knowledge of front-end JavaScript and browser APIs as well as significant experience with popular frameworks and libraries like React and Redux.',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/drodva/',
+    github: 'https://github.com/devDrodva',
+  },
+}
+
+const projects = [
+  {
+    name: 'Ecommerce',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['React', 'JavaScript', 'MongoDB'],
+    sourceCode: 'https://github.com/devDrodva/ecommerce',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'NextJs',
+  'NodeJs',
+  'GraphQL',
+  'MongoDB',
+  'Git',
+  'Jest',
+]
+
+const contact = {
+  email: 'dvargas0720@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
